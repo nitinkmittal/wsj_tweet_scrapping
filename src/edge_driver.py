a8c2c7820d33bc53a7aa6b07820acf93116d61d3
nitinkumarmittal <nitinmittal243@gmail.com>
2020-11-15 20:31:28 -0500

    Added support to extract WSJ tweets using Twint and extracted and cleaned news articles and identified companies in news articles. Identified disruption keywords from news articles.

---
+++ src/edge_driver.py
@@ -0,0 +1,41 @@
+# works with Microsoft Edge and it's driver
+from selenium import webdriver
+from selenium.webdriver.edge.webdriver import WebDriver
+from bs4 import BeautifulSoup
+
+
+def open_driver():
+    """
+    Open and return driver.
+    
+    Note : Path tp Microsoft Edge driver should be set correctly.
+    """
+    driver_path = r"C:\Users\user\Downloads\Softwares\edgedriver_win64\msedgedriver.exe"
+    return webdriver.Edge(driver_path)
+
+
+def load_url(driver : WebDriver,
+             url : str,
+             verbose : int = 0):
+    """
+    Load URL using driver.
+    
+    Parameters
+    ----------
+    driver : selenium.edge.webdriver.
+    
+    url : str.
+    
+    verbose : boolean, default=False
+        if True, print function log.
+    """
+    if verbose >= 5: print("Loading page")
+    try:
+        driver.get(url) # loading page
+    except Exception as e:
+        print(f"Failed loading page for url : {url}, exception : {e}")
+        
+        
+def get_driver_page_source(driver : WebDriver):
+    """Extract page source and return BeautifulSoup object."""
+    return BeautifulSoup(driver.page_source, "lxml")
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,33 @@
 from nltk.corpus import stopwords
 from typing import Dict, List
 import re
+from pickle import load, dump, HIGHEST_PROTOCOL
+from os import mkdir
 
+
+def dump_pickle(obj, filename : str):
+    """Save object in Python pickle form."""
+    dump(obj, open(f"{filename}.pickle", "wb"),
+                   protocol=HIGHEST_PROTOCOL)
+    
+def load_pickle(filename : str):
+    """Load Python pickle.""" 
+    if ".pickle" in filename:
+        filename = filename.replace(".pickle","")
+    return load(open(f"{filename}.pickle", "rb"))
+
+def create_dir(folder_name : str):
+    """
+    Create folder if not already exists.
+    
+    Assumes folder path is appropriately attached to folder_name.
+    """
+    try:
+        mkdir(folder_name)
+    except Exception as e:
+        print(f"Failed creating directory, exception : {e}")
+    else:
+        print("Successfully created directory")
 
 def get_stopwords(lang : str = "english"):
     return stopwords.words(lang)
